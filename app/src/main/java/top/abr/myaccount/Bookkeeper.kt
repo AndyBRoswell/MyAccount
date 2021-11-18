0f8034e899611e2f6da89b8a754e981d051c804a
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-18 12:27:04 +0800

    Update Bookkeeper.kt

--- app/src/main/java/top/abr/myaccount/Bookkeeper.kt
+++ app/src/main/java/top/abr/myaccount/Bookkeeper.kt
@@ -1,20 +1,33 @@
 package top.abr.myaccount
 
+import java.util.Currency
+import java.time.ZonedDateTime
 import java.util.*
+import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 
+typealias IDType = Long
+typealias IDTypeCollection = TreeSet<IDType>
+typealias AccountType = String
+typealias LabelType = String
+
 class Bookkeeper {
-	inner class InvolvedItem {
-		var Place: String
+	open inner class InvolvedItem {
 		var ItemName: String
-		var InvolvedAccount: String
-		var Amount: Double
+		var Place: String
+		var AccountUsed: String
 		var Label: HashSet<String>
 		var Details: String
+		var OriginalCurrency: Currency
+		var OriginalPrice: Double
+		var ExchangeRate: Double
 	}
 
 	companion object {
-		private val ItemByID: MutableMap<Long, InvolvedItem> = TreeMap()
-		private val 
+		private val ItemByID: MutableMap<IDType, InvolvedItem> = TreeMap()
+		private val IDByDateTime: TreeMap<ZonedDateTime, IDTypeCollection> = TreeMap()
+		private val IDByLabel: HashMap<LabelType, IDTypeCollection> = HashMap()
+		private val IDByAccount: HashMap<AccountType, IDTypeCollection> = HashMap()
+		private val DefaultCurrency: HashMap<AccountType, Currency> = HashMap()
 	}
 }
