86359811d1b1289677ff4bed6ae7b18d69e49e8f
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-18 21:40:06 +0800

    adding synonym dict

--- app/src/main/java/top/abr/myaccount/SynonymDictionary.kt
+++ app/src/main/java/top/abr/myaccount/SynonymDictionary.kt
@@ -1,4 +1,53 @@
 package top.abr.myaccount
 
 class SynonymDictionary {
+	private val CanonicalWord: MutableMap<String, String> = HashMap()
+	private val NonCanonicalWords: MutableMap<String, HashSet<String>> = HashMap()
+
+	fun Insert(Word: String, Synonym: String) {
+		val CWord = CanonicalWord[Word]
+		if (CWord == null) {														// The param Word is not in the dict yet.
+			CanonicalWord[Word] = Word												// Use this word itself as the canonical word
+			if (Word != Synonym) NonCanonicalWords[Word] = hashSetOf(Synonym)
+			else NonCanonicalWords[Word] = HashSet()								// Elide the repeated word
+		}
+		else {
+			
+			NonCanonicalWords[Word]!!.add(Synonym)
+		}
+	}
+
+	fun Insert(Word: String, Synonyms: Iterable<String>) {
+		val CWord = CanonicalWord[Word]
+		if (CWord == null) {
+			CanonicalWord[Word] = Word
+			val NCWords = HashSet<String>()
+			for (Synonym in Synonyms) NCWords.add(Synonym)
+			NonCanonicalWords[Word] = NCWords
+		}
+		else {
+			val NCWords = NonCanonicalWords[CWord]
+			for (Synonym in Synonyms) NCWords!!.add(Synonym)
+		}
+	}
+
+	fun Delete(Word: String, Synonym: String) {
+
+	}
+
+	fun Delete(Word: String, Synonyms: Iterable<String>) {
+
+	}
+
+	fun Modify(Word: String, OldSynonym: String, NewSynonym: String) {
+
+	}
+
+	fun Modify(Word: String, OldSynonyms: Iterable<String>, NewSynonyms: Iterable<String>) {
+
+	}
+
+	fun Find(Word: String): Map.Entry<String, String>? {
+		return null
+	}
 }
