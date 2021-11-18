640fe0d3f9896c7813bc3b18b7cc857b21bc14f9
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-18 22:10:19 +0800

    Update SynonymDictionary.kt

--- app/src/main/java/top/abr/myaccount/SynonymDictionary.kt
+++ app/src/main/java/top/abr/myaccount/SynonymDictionary.kt
@@ -6,21 +6,21 @@ class SynonymDictionary {
 
 	fun Insert(Word: String, Synonym: String) {
 		val CWord = CanonicalWord[Word]
-		if (CWord == null) { // The param Word is not in the dict yet.
-			CanonicalWord[Word] = Word												// Use the param Word itself as the canonical word
-			if (Word != Synonym) NonCanonicalWords[Word] = hashSetOf(Synonym)
-			else NonCanonicalWords[Word] = HashSet()								// Elide the repeated element Word
+		if (CWord == null) { // The param Word is not in the dict yet
+			CanonicalWord[Word] = Word // Use the param Word itself as the canonical word
+			if (Word != Synonym) NonCanonicalWords[Word] = hashSetOf(Synonym)    // The other word Synonym as the non-canonical aliases
+			else NonCanonicalWords[Word] = HashSet()                            // Elide the repeated element Word
 		}
 		else if (CWord == Word) {
-			if (Word != Synonym) NonCanonicalWords[CWord]!!.add(Synonym)			// Elide the repeated element Word
-			else return 															// No synonyms to add. Note: Canonical word should NOT appear in the value of the K-V pair of NonCanonicalWords
+			if (Word != Synonym) NonCanonicalWords[CWord]!!.add(Synonym)            // Elide the repeated element Word
+			else return                                                            // No synonyms to add. Note: Canonical word should NOT appear in the value of K-V pairs of NonCanonicalWords
 		}
 		else if (CWord == Synonym) {
-			if (Word != Synonym) NonCanonicalWords[CWord]!!.add(Word)				// Elide the repeated element Word
-			else return																// No synonyms to add.
+			if (Word != Synonym) NonCanonicalWords[CWord]!!.add(Word)                // Elide the repeated element Word
+			else return                                                                // No synonyms to add
 		}
-		else {
-			NonCanonicalWords[CWord]!!.add(Synonym)									// Regardless of whether Word == Synonym
+		else { // CWord != Word and CWord != Synonym
+			NonCanonicalWords[CWord]!!.add(Synonym)                                    // Regardless of whether Word == Synonym
 		}
 	}
 
@@ -29,12 +29,19 @@ class SynonymDictionary {
 		if (CWord == null) {
 			CanonicalWord[Word] = Word
 			val NCWords = HashSet<String>()
-			for (Synonym in Synonyms) NCWords.add(Synonym)
+			for (Synonym in Synonyms) {
+				if (Word != Synonym) NCWords.add(Synonym)        // Elide the repeated element Word
+			}
 			NonCanonicalWords[Word] = NCWords
 		}
-		else {
+		else if (CWord == Word) {
 			val NCWords = NonCanonicalWords[CWord]
-			for (Synonym in Synonyms) NCWords!!.add(Synonym)
+			for (Synonym in Synonyms) {
+
+			}
+		}
+		else {
+
 		}
 	}
 
