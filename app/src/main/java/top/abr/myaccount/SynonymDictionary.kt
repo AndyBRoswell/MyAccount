342f532f4238db71758abe86ea4d53f74b1929ce
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-19 17:02:59 +0800

    Update SynonymDictionary.kt

--- app/src/main/java/top/abr/myaccount/SynonymDictionary.kt
+++ app/src/main/java/top/abr/myaccount/SynonymDictionary.kt
@@ -9,15 +9,22 @@ open class SynonymDictionary {
         val NO_ID = 0L
     }
 
-    private val CanonicalID: MutableMap<String, Long> = HashMap()
+    private val CanonicalID: MutableMap<String, Long> = HashMap()       // These IDs are for internal usage ONLY
     private val Synonyms: MutableMap<Long, HashSet<String>> = TreeMap()
 
     fun Insert(Word: String, Synonym: String) {
-
+        var CID = CanonicalID[Word]
+        if (CID == null) CID = GenerateCanonicalID()    // Allocate a new canonical ID for the specified word if it doesn't appear in this dict
+        CanonicalID[Synonym] = CID                      // The canonical ID of the synonym is identical to the canonical ID of the specified word
+        Synonyms[CID]!!.add(Synonym)
     }
 
     fun Insert(Word: String, Synonyms: Iterable<String>) {
-
+        var CID = CanonicalID[Word]
+        if (CID == null) CID = GenerateCanonicalID()    // Allocate a new canonical ID for the specified word if it doesn't appear in this dict
+        for (Synonym in Synonyms) {
+            
+        }
     }
 
     fun Delete(Word: String, Synonym: String) {
@@ -31,4 +38,6 @@ open class SynonymDictionary {
     fun Find(Word: String): Iterable<String>? = Synonyms[CanonicalID[Word]]
 
     fun GetCanonicalID(Word: String) = CanonicalID[Word]
+
+    private fun GenerateCanonicalID(): Long = System.nanoTime()
 }
