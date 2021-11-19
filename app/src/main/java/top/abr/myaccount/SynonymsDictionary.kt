04db517a0e41219fc5981accb247b5d88beefc7b
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-19 17:22:52 +0800

    a new ver of synonym dict

--- app/src/main/java/top/abr/myaccount/SynonymsDictionary.kt
+++ app/src/main/java/top/abr/myaccount/SynonymsDictionary.kt
@@ -4,14 +4,15 @@ import java.util.*
 import kotlin.collections.HashMap
 import kotlin.collections.HashSet
 
-open class SynonymDictionary {
-    companion object {
-        val NO_ID = 0L
-    }
-
+open class SynonymsDictionary {
     private val CanonicalID: MutableMap<String, Long> = HashMap()       // These IDs are for internal usage ONLY
     private val Synonyms: MutableMap<Long, HashSet<String>> = TreeMap()
 
+    /**
+     * Add a synonym for the specified word.
+     * @param Word The word you want to add a new synonym for.
+     * @param Synonym The synonym you want to add for <code>Word</code>.
+     */
     fun Insert(Word: String, Synonym: String) {
         var CID = CanonicalID[Word]
         if (CID == null) { // Allocate a new canonical ID for the specified word if it doesn't appear in this dict
@@ -22,6 +23,11 @@ open class SynonymDictionary {
         Synonyms[CID]!!.add(Synonym)
     }
 
+    /**
+     * Add a group of synonyms for the specified word.
+     * @param Word The word you want to add a new synonym for.
+     * @param Synonyms The group of synonyms you want to add for <code>Word</code>.
+     */
     fun Insert(Word: String, Synonyms: Iterable<String>) {
         var CID = CanonicalID[Word]
         if (CID == null) { // Allocate a new canonical ID for the specified word if it doesn't appear in this dict
@@ -34,10 +40,20 @@ open class SynonymDictionary {
         }
     }
 
+    /**
+     * Delete a synonym for the specified word.
+     * @param Word The word you want to delete a synonym for.
+     * @param Synonym The synonym you want to add for <code>Word</code>.
+     */
     fun Delete(Word: String, Synonym: String) {
 
     }
 
+    /**
+     * Delete a group of synonyms for the specified word.
+     * @param Word The word you want to delete synonyms for.
+     * @param Synonyms The group of synonyms you want to delete for <code>Word</code>.
+     */
     fun Delete(Word: String, Synonyms: Iterable<String>) {
 
     }
