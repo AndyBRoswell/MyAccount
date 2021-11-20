d246dec10db3b92ba3696a20cc72476daee0ef66
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-21 00:28:57 +0800

    Update SynonymDictionaryUnitTest.kt

--- app/src/test/java/top/abr/myaccount/SynonymDictionaryUnitTest.kt
+++ app/src/test/java/top/abr/myaccount/SynonymDictionaryUnitTest.kt
@@ -3,9 +3,7 @@ package top.abr.myaccount
 import org.apache.commons.lang3.RandomStringUtils
 import org.junit.Assert.*
 import org.junit.Test
-import java.lang.AssertionError
 import java.security.SecureRandom
-import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashSet
 import kotlin.math.pow
@@ -28,17 +26,15 @@ class SynonymDictionaryUnitTest {
         val MIN_SYNONYM_COUNT = 10
         val MAX_WORD_LENGTH = 1
         val MIN_WORD_LENGTH = 1
-
         // MAX_SYNONYM_COUNT must be less than (C ^ MIN_WORD_LENGTH), should be MUCH LESS THAN (C ^ MIN_WORD_LENGTH), C = 52 when randomAlphabetic() is used
         assertTrue(MAX_SYNONYM_COUNT < 52.0.pow(MIN_WORD_LENGTH.toDouble()))
-
         val SynonymGroupCount = NextIntRInclusive(MIN_SYNONYM_GROUP_COUNT, MAX_SYNONYM_GROUP_COUNT)
         val SynonymCounts = RandomSource.ints(SynonymGroupCount.toLong(), MIN_SYNONYM_COUNT, MAX_SYNONYM_COUNT).toArray()
 
         // Round 1: Insert(Word: String, Synonym: String)
         val SDict = SynonymDictionary()
         val SSets = ArrayList<HashSet<String>>()
-        val SLists = ArrayList<ArrayList<String>>()
+        val SList = ArrayList<ArrayList<String>>()
         val TestCount = ArrayList<ArrayList<Int>>()
         val UncoveredSynonymCount = ArrayList<Int>()
         // Prepare test data
@@ -46,15 +42,24 @@ class SynonymDictionaryUnitTest {
             val SSet = HashSet<String>()
             while (SSet.size < SynonymCount) SSet.add(RandomStringRInclusive(MIN_WORD_LENGTH, MAX_WORD_LENGTH))
             SSets.add(SSet)
-            val SList = ArrayList<String>(SSet)
-            SLists.add(SList)
-            UncoveredSynonymCount.add(SList.size)
-            TestCount.add(ArrayList<Int>().apply { for (i in 1..SList.size) add(0) })
+            val CurrentSList = ArrayList<String>(SSet)
+            SList.add(CurrentSList)
+            UncoveredSynonymCount.add(CurrentSList.size)
+            TestCount.add(ArrayList<Int>().apply { for (i in 1..CurrentSList.size) add(0) })
         }
         // Start to insert
-        for (SList in SLists) {
-            
+        for (i in SList.indices) {
+            while (UncoveredSynonymCount[i] > 0) {
+                val WordIndex = NextInt(0, SList[i].size)
+                val SynonymIndex = NextInt(0, SList[i].size)
+                if (TestCount[i][WordIndex] == 0) --UncoveredSynonymCount[i]
+                if (TestCount[i][SynonymIndex] == 0) --UncoveredSynonymCount[i]
+                SDict.Insert(SList[i][WordIndex], SList[i][SynonymIndex])
+                ++TestCount[i][WordIndex]
+                ++TestCount[i][SynonymIndex]
+            }
         }
         // Start to verify
+        
     }
 }
