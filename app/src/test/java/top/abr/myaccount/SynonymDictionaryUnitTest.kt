51d0a33ca899717a58a239132d95deb546d19ebc
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-22 13:24:01 +0800

    Update SynonymDictionaryUnitTest.kt

--- app/src/test/java/top/abr/myaccount/SynonymDictionaryUnitTest.kt
+++ app/src/test/java/top/abr/myaccount/SynonymDictionaryUnitTest.kt
@@ -20,15 +20,15 @@ class SynonymDictionaryUnitTest {
 
     @Test fun InsertTest() {
         // Range parameters of random data
-        val MAX_SYNONYM_GROUP_COUNT = 20
-        val MIN_SYNONYM_GROUP_COUNT = 10
+        val MAX_SYNONYM_GROUP_COUNT = 2
+        val MIN_SYNONYM_GROUP_COUNT = 1
         val MAX_SYNONYM_GROUP_SIZE = 20
         val MIN_SYNONYM_GROUP_SIZE = 10
-        val MAX_WORD_LENGTH = 2
+        val MAX_WORD_LENGTH = 1
         val MIN_WORD_LENGTH = 1
-        // MAX_SYNONYM_GROUP_SIZE must be less than pow(C, MAX_WORD_LENGTH), should be MUCH LESS THAN pow(C, MAX_WORD_LENGTH), C = 52 when randomAlphabetic() is used
+        // MAX_SYNONYM_GROUP_SIZE * MAX_SYNONYM_GROUP_COUNT must be less than pow(C, MAX_WORD_LENGTH), should be MUCH LESS THAN pow(C, MAX_WORD_LENGTH), C = 52 when randomAlphabetic() is used
         val MAX_ALL_SYNONYMS_COUNT = 52.0.pow(MAX_WORD_LENGTH.toDouble()).toInt()
-        assertTrue(MAX_SYNONYM_GROUP_SIZE < MAX_ALL_SYNONYMS_COUNT)
+        assertTrue(MAX_SYNONYM_GROUP_SIZE * MAX_SYNONYM_GROUP_COUNT < MAX_ALL_SYNONYMS_COUNT)
         val SynonymGroupCount = NextIntRClosed(MIN_SYNONYM_GROUP_COUNT, MAX_SYNONYM_GROUP_COUNT)
         val SynonymCount = RandomIntArrayRClosed(SynonymGroupCount.toLong(), MIN_SYNONYM_GROUP_SIZE, MAX_SYNONYM_GROUP_SIZE)
 
@@ -36,19 +36,14 @@ class SynonymDictionaryUnitTest {
         val SDict = SynonymDictionary()
         val SynonymsForTest = HashSet<String>()
         val SList = ArrayList<ArrayList<String>>()
-        val TestCount = ArrayList<ArrayList<Int>>()
-        val UncoveredSynonymCount = ArrayList<Int>()
         // Prepare test data
         for (i in SynonymCount.indices) {
             SList.add(ArrayList())
             while (SList[i].size < SynonymCount[i]) {
                 val Synonym = RandomStringRClosed(MIN_WORD_LENGTH, MAX_WORD_LENGTH)
                 // SynonymsForTest.size must be less than pow(C, MAX_WORD_LENGTH), should be MUCH LESS THAN pow(C, MAX_WORD_LENGTH), C = 52 when randomAlphabetic() is used
-                if (SynonymsForTest.add(Synonym)) SList[i].add(Synonym)                 // Each synonym which will be added for tests is different from others
-                else assertTrue(SynonymsForTest.size < MAX_ALL_SYNONYMS_COUNT)
+                if (SynonymsForTest.add(Synonym)) SList[i].add(Synonym)         // Each synonym which will be added for tests is different from others
             }
-            UncoveredSynonymCount.add(SynonymCount[i])
-            TestCount.add(ArrayList<Int>().apply { for (j in 1..SynonymCount[i]) add(0) })
         }
         // Start to insert
         for (i in SList.indices) { // Every synonym in SList is guaranteed to be the arg of SynonymDictionary.insert() at least once.
@@ -63,10 +58,10 @@ class SynonymDictionaryUnitTest {
                 }
             }
             else Interval.add(Pair(0, 0))
-            for (CurrentInterval in Interval) {                                                           // Usual insertion test
+            for (CurrentInterval in Interval) { // Usual insertion test
                 for (j in 0 until CurrentInterval.second) SDict.Insert(SList[i][j], SList[i][j + 1])
             }
-            for (j in 0 until Interval.size - 1) {                                          // Merge test
+            for (j in 0 until Interval.size - 1) { // Merge test
                 val WordIndex = NextIntRClosed(Interval[j].first, Interval[j].second)
                 val SynonymIndex = NextIntRClosed(Interval[j + 1].first, Interval[j + 1].second)
                 SDict.Insert(SList[i][WordIndex], SList[i][SynonymIndex])
