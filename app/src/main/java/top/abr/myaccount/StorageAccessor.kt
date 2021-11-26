373aa7eb54c26d0bc2c002d9707e91faeacb2449
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-11-26 16:57:38 +0800

    Update StorageAccessor.kt

--- app/src/main/java/top/abr/myaccount/StorageAccessor.kt
+++ app/src/main/java/top/abr/myaccount/StorageAccessor.kt
@@ -1,4 +1,8 @@
 package top.abr.myaccount
 
+import com.dslplatform.json.DslJson
+import com.dslplatform.json.runtime.Settings
+
 open class StorageAccessor {
+    val JSONProcessor: DslJson<Any> = DslJson(Settings.withRuntime<Any>().includeServiceLoader().allowArrayFormat(true))
 }
