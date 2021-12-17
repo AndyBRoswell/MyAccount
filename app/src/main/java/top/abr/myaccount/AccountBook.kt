2ddf38f60597a1fe231589847d45251c79640be4
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-17 17:44:30 +0800

    /

--- app/src/main/java/top/abr/myaccount/AccountBook.kt
+++ app/src/main/java/top/abr/myaccount/AccountBook.kt
@@ -11,8 +11,10 @@ typealias AccountID = Long
 typealias LabelType = String
 
 open class AccountBook {
+    // Main records
     private val ItemByID: MutableMap<ItemID, ItemPurchased> = TreeMap()                 // ID as primary key for each item purchased
     private val DefaultCurrency: MutableMap<AccountID, Currency> = HashMap()            // Default currency of accounts
+    // Extra indices for quick search
     private val IDByDateTime: MutableMap<ZonedDateTime, IDCollection> = TreeMap()       // Index item ID by purchase time. Typically for the single purchase of multiple items.
     private val IDByAccount: MutableMap<AccountID, IDCollection> = HashMap()            // Index item ID by account.
     private val IDByLabel: MutableMap<LabelType, IDCollection> = HashMap()              // Index item ID by label.
