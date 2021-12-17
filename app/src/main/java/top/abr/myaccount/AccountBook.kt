805b7104b05ddb16f89596a5383a86a32663fc6f
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-17 17:17:36 +0800

    Update AccountBook.kt

--- app/src/main/java/top/abr/myaccount/AccountBook.kt
+++ app/src/main/java/top/abr/myaccount/AccountBook.kt
@@ -16,7 +16,7 @@ open class AccountBook {
         var Name: String = ""                                                           // The name of the item purchased
         var Details: String = ""                                                        // The details of this item
         var Labels: MutableSet<String> = HashSet()                                      // The labels of this item. These labels are used for quick search.
-        var Place: String = ""                                                          //
+        var Site: String = ""                                                           // The site where this item was purchased
         var Account: String = ""                                                        // The account used to pay for this purchase
         var OriginalCurrency: Currency = Currency.getInstance(Locale.getDefault())      // The currency used by the vendor of this item
         var OriginalPrice: Double = 0.0                                                 // The nominal price given by the vendor using the specific (original) currency
