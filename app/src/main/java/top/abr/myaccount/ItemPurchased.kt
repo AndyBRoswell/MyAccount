ceda350ee70c4fc26751ada2cae956cbe25f5725
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-17 17:39:52 +0800

    Update ItemPurchased.kt

--- app/src/main/java/top/abr/myaccount/ItemPurchased.kt
+++ app/src/main/java/top/abr/myaccount/ItemPurchased.kt
@@ -7,7 +7,7 @@ open class ItemPurchased {
     var Name: String = ""                                                           // The name of the item purchased
     var Details: String = ""                                                        // The details of this item
     var Labels: MutableSet<String> = HashSet()                                      // The labels of this item. These labels are used for quick search.
-    var Site: String = ""                                                           // The site where this item was purchased
+    var Site: String = ""                                                           // The site (typically bricks and mortar, or websites) where this item was purchased
     var Account: String = ""                                                        // The account used to pay for this purchase
     var OriginalCurrency: Currency = Currency.getInstance(Locale.getDefault())      // The currency used by the vendor of this item
     var OriginalPrice: Double = 0.0                                                 // The nominal price given by the vendor using the specific (original) currency
