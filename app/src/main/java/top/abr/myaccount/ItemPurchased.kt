a4a9b56e10157743b44c3b2cf0056f072825782e
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-18 11:49:22 +0800

    /

--- app/src/main/java/top/abr/myaccount/ItemPurchased.kt
+++ app/src/main/java/top/abr/myaccount/ItemPurchased.kt
@@ -11,7 +11,6 @@ open class ItemPurchased {
     var Site: String = ""                                                           // The site (typically bricks and mortar, or websites) where this item was purchased
     var Account: String = ""                                                        // The account used to pay for this purchase
     var PurchaseTime: ZonedDateTime = ZonedDateTime.now()                           // The time when this item was purchased
-    
     var OriginalCurrency: Currency = Currency.getInstance(Locale.getDefault())      // The currency used by the vendor of this item
     var OriginalPrice: Double = 0.0                                                 // The nominal price given by the vendor using the specific (original) currency
     var ExchangeRate: Double = 1.0                                                  // The exchange rate from the original currency to the default currency of the account used
--- app/src/main/java/top/abr/myaccount/MainActivity.kt
+++ app/src/main/java/top/abr/myaccount/MainActivity.kt
@@ -3,11 +3,15 @@ package top.abr.myaccount
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import top.abr.myaccount.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    lateinit var ActivityMain: ActivityMainBinding
+
+    override fun onCreate(SavedInstanceState: Bundle?) {
+        super.onCreate(SavedInstanceState)
+        ActivityMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ActivityMain.root)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)		// Dark theme as default
     }
