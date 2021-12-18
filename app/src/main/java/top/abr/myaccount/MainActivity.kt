505d8d6cded021fda3062a9bd6c9f955e3ba567f
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-18 21:21:56 +0800

    added main navigation drawer menu

--- app/src/main/java/top/abr/myaccount/MainActivity.kt
+++ app/src/main/java/top/abr/myaccount/MainActivity.kt
@@ -2,6 +2,7 @@ package top.abr.myaccount
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.drawerlayout.widget.DrawerLayout
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(SavedInstanceState: Bundle?) {
         super.onCreate(SavedInstanceState)
+
         // Inflate
         ActivityMain = ActivityMainBinding.inflate(layoutInflater)
         setContentView(ActivityMain.root)
@@ -21,14 +23,20 @@ class MainActivity : AppCompatActivity() {
             NavDrawerListener = ActionBarDrawerToggle(this@MainActivity, this, R.string.open_nav_drawer, R.string.close_nav_drawer)
             addDrawerListener(NavDrawerListener) // pass the Open and Close toggle for the drawer layout listener to toggle the button
         }
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // To make the Navigation drawer icon always appear on the action bar
+
         // Theme
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)		// Dark theme as default
-        // Main navigation drawer
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) // Dark theme as default
     }
 
     override fun onPostCreate(SavedInstanceState: Bundle?) {
         super.onPostCreate(SavedInstanceState)
-        NavDrawerListener.syncState()
+        NavDrawerListener.syncState() // Synchronize the indicator with the state of the linked DrawerLayout after onRestoreInstanceState has occurred.
+    }
+
+    // Open and close the navigation drawer when the icon is clicked
+    override fun onOptionsItemSelected(MenuItem: MenuItem): Boolean {
+        if (NavDrawerListener.onOptionsItemSelected(MenuItem)) return true
+        return super.onOptionsItemSelected(MenuItem)
     }
 }
