3678d9eef6ad31b65d2c6cd3280eb7d865859320
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-19 11:50:10 +0800

    /

---
+++ app/src/main/java/top/abr/myaccount/ItemsPurchasedAdapter.kt
@@ -0,0 +1,22 @@
+package top.abr.myaccount
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+open class ItemsPurchasedAdapter(): RecyclerView.Adapter<ItemsPurchasedAdapter.ItemPurchasedViewHolder>() {
+    open inner class ItemPurchasedViewHolder(): RecyclerView.ViewHolder() {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemPurchasedViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ItemPurchasedViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
