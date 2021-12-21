dd7cf18ed99dd2b524a9abd169a1f23ad8c321bc
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-21 17:40:25 +0800

    /

--- app/src/main/java/top/abr/myaccount/ItemsPurchasedAdapter.kt
+++ app/src/main/java/top/abr/myaccount/ItemsPurchasedAdapter.kt
@@ -1,18 +1,20 @@
 package top.abr.myaccount
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 open class ItemsPurchasedAdapter(): RecyclerView.Adapter<ItemsPurchasedAdapter.ItemPurchasedViewHolder>() {
-    open inner class ItemPurchasedViewHolder(): RecyclerView.ViewHolder() {
+    open inner class ItemPurchasedViewHolder(ItemView: View): RecyclerView.ViewHolder(ItemView) {
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemPurchasedViewHolder {
-
+    override fun onCreateViewHolder(Parent: ViewGroup, ViewType: Int): ItemPurchasedViewHolder {
+        return ItemPurchasedViewHolder(LayoutInflater.from(Parent.context).inflate(R.layout.item_purchased_layout, Parent, false))
     }
 
-    override fun onBindViewHolder(holder: ItemPurchasedViewHolder, position: Int) {
+    override fun onBindViewHolder(Holder: ItemPurchasedViewHolder, Position: Int) {
 
     }
 
