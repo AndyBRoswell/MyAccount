f80cf279e570aa0cf166e056c0bd7127b7be8bec
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-21 20:02:55 +0800

    /

--- app/src/main/java/top/abr/myaccount/ItemsPurchasedAdapter.kt
+++ app/src/main/java/top/abr/myaccount/ItemsPurchasedAdapter.kt
@@ -3,11 +3,17 @@ package top.abr.myaccount
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 open class ItemsPurchasedAdapter(): RecyclerView.Adapter<ItemsPurchasedAdapter.ItemPurchasedViewHolder>() {
     open inner class ItemPurchasedViewHolder(ItemView: View): RecyclerView.ViewHolder(ItemView) {
-
+        val NameView: TextView = ItemView.findViewById(R.id.ItemPurchasedLayoutName)
+        val DateTimeView: TextView = ItemView.findViewById(R.id.ItemPurchasedLayoutDateTime)
+        val SiteView: TextView = ItemView.findViewById(R.id.ItemPurchasedLayoutSite)
+        val AccountView: TextView = ItemView.findViewById(R.id.ItemPurchasedLayoutAccount)
+        val OriginalPriceView: TextView = ItemView.findViewById(R.id.ItemPurchasedLayoutOriginalPrice)
+        val PriceView: TextView = ItemView.findViewById(R.id.ItemPurchasedLayoutPrice)
     }
 
     override fun onCreateViewHolder(Parent: ViewGroup, ViewType: Int): ItemPurchasedViewHolder {
