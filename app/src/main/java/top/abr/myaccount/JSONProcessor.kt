88d370289cabe66d36d57666ff0ba1d02152d651
Andy Bayer Roswell <andyroswellrr@gmail.com>
2021-12-26 15:42:34 +0800

    Update JSONProcessor.kt

--- app/src/main/java/top/abr/myaccount/JSONProcessor.kt
+++ app/src/main/java/top/abr/myaccount/JSONProcessor.kt
@@ -2,9 +2,16 @@ package top.abr.myaccount
 
 import com.dslplatform.json.DslJson
 import com.dslplatform.json.runtime.Settings
+import java.io.ByteArrayOutputStream
 
 class JSONProcessor {
     companion object {
-        private val JSONProcessor: DslJson<Any> = DslJson(Settings.withRuntime<Any>().includeServiceLoader().allowArrayFormat(true))
+        private val Core: DslJson<Any> = DslJson(Settings.withRuntime<Any>().includeServiceLoader().allowArrayFormat(true))
+
+        fun Serialize(Instance: Any): String {
+            val ostream = ByteArrayOutputStream()
+            Core.serialize(Instance, ostream)
+            return ostream.toString()
+        }
     }
 }
